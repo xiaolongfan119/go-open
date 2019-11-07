347ee9916637ab5a1b970c450841685479bd9efa
hai.zhang <hai.zhang@mail.haoyisheng.com>
2019-11-07 11:11:07 +0800

    InSliceInt

--- library/util/slice.go
+++ library/util/slice.go
@@ -9,3 +9,23 @@ func InSlice(slice []interface{}, target interface{}) bool {
 	}
 	return false
 }
+
+func InSliceInt(slice []int, target int) bool {
+
+	for _, v := range slice {
+		if v == target {
+			return true
+		}
+	}
+	return false
+}
+
+func InSliceString(slice []string, target string) bool {
+
+	for _, v := range slice {
+		if v == target {
+			return true
+		}
+	}
+	return false
+}
